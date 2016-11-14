d5f790d1c14323986c5685147eb5b1cfea83c32c
Mani <Administrator@DEXTER>
2016-11-14 18:43:03 -0500

    Let the development begin

---
+++ CarPool/Models/Pool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarPool.Models
+{
+    public class Pool
+    {
+        public string name { get; set; }
+        public string host { get; set; }
+        public string fromAddress { get; set; }
+        public string toAddress { get; set; }
+        public string carType { get; set; }
+        public string carNumber { get; set; }
+        public int seatsToOffer{get;set;}
+        public DateTime date { get; set; }
+        public DateTime startTime { get; set; }
+
+    }
+}
