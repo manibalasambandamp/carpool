881d4f6b2dd67fc428e91a9596a996da8fbe74f9
Mani <Administrator@DEXTER>
2016-11-21 20:54:24 -0500

    Azure test

---
+++ CarPool/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarPool.Models
+{
+    public class Address
+    {
+        public string door;
+        public string street;
+        public string city;
+        public string state;
+        public string country;
+        public int zip;
+    }
+}
--- CarPool/Models/Pool.cs
+++ CarPool/Models/Pool.cs
@@ -7,15 +7,18 @@ namespace CarPool.Models
 {
     public class Pool
     {
-        public string name { get; set; }
-        public string host { get; set; }
+        public string poolName { get; set; }
+        public string hostName { get; set; }
         public string fromAddress { get; set; }
         public string toAddress { get; set; }
+        public string fromPin { get; set; }
+        public string toPin { get; set; }
         public string carType { get; set; }
         public string carNumber { get; set; }
         public int seatsToOffer{get;set;}
         public DateTime date { get; set; }
         public DateTime startTime { get; set; }
 
+        public List<PoolMember> members;
     }
 }
